bf2c925790499998901b79f6e6ab7b34b8c0fef6
SimonAhl <siah04001@utb.vaxjo.se>
2022-04-02 19:30:50 +0200

    Började på fiender
    Skapade en skelett sprite och lade in den med en simpel movement script

---
+++ Slutprojekt/Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+
+    public Transform playerpos;
+    public SpriteRenderer spriteR;
+    [SerializeField] float speed;
+    void Update()
+    {
+        //Sätter objektets rotation till att kolla rakt på spelaren
+        transform.LookAt(playerpos);
+
+        //Rör objektet framåt genom att ta framåt vectorn gånger speed variabeln gånger "Time.deltatime"
+        //Jag använder Time.deltatime här för att se till så att farten är konstant oberoende av framerate
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        //Sedan sätts objektets rotation till 0, annars händer konstiga saker med spriten
+        transform.rotation = Quaternion.identity;
+
+        //Här flippas objektets sprite så att den alltid kollar mot spelaren
+        if(playerpos.position.x >= transform.position.x){
+           spriteR.flipX = true;
+        }
+        else{
+           spriteR.flipX = false;
+        }
+    }
+}
